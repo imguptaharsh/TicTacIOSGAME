b1311fc54cfbada1e18d2b8cbd4d65dc16fe928a
Harsh Gupta <imguptaharsh25@gmail.com>
2023-04-13 23:17:12 +0530

    Creating IOS app for TicTacGame

---
+++ TicTacIOSGame/TicTacIOSGame/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TicTacIOSGame
+//
+//  Created by Harsh Gupta on 13/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TicTacIOSGame/TicTacIOSGame/TicTacIOSGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacIOSGameApp.swift
+//  TicTacIOSGame
+//
+//  Created by Harsh Gupta on 13/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacIOSGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
